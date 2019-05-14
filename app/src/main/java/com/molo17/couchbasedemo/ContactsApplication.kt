52f0463b176b8e77c85248f955f54f961e2d2174
Matteo Sist <matteo.sist@molo17.com>
2019-05-14 14:56:35 +0200

    source code

---
+++ app/src/main/java/com/molo17/couchbasedemo/ContactsApplication.kt
@@ -0,0 +1,30 @@
+package com.molo17.couchbasedemo
+
+import android.app.Application
+
+/**
+ * Created by Matteo Sist on 01/03/2019.
+ */
+class ContactsApplication : Application() {
+
+    /// STEP 39
+    /// Declare replication manager variable.
+//    private var replicatorManager: ReplicatorManager? = null
+
+    override fun onCreate() {
+        super.onCreate()
+
+        /// STEP 45
+        /// Check if url string is valid and init replicationManager.
+//        replicatorManager = runCatching { URI(remoteDatabaseURL) }.getOrNull()
+//            ?.takeIf { it.toString().isNotEmpty() }
+//            ?.let { ReplicatorManager(this, it) }
+
+            /// STEP 46
+            /// Start replication when app enter in foreground.
+            /// Stop replication when app enter in background.
+//            ?.also(ProcessLifecycleOwner.get().lifecycle::addObserver)
+    }
+
+
+}
